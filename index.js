f4131b7866ab651ca99b068506dd69381d0f0306
pemrouz <p3pedram@hotmail.com>
2015-07-09 00:14:31 +0100

    skip non-strings

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var to = require('to')
+  , is = require('is')
 
 module.exports = function grep(o, k, regex){
   var original = o[k] 
   o[k] = function(){ 
-    var d = to.arr(arguments).join(' ')
+    var d = to.arr(arguments).filter(is.str).join(' ')
     return d.match(regex) && original.apply(this, arguments) 
   }
   return original
--- test.js
+++ test.js
@@ -15,4 +15,15 @@ describe('grep', function() {
     expect(o.fn('bar sth')).to.equal('bar sth')
   })
 
+  it('should skip non-string arguments', function() {
+    var o = { fn: identity }
+      , all = grep(o, 'fn', /^(?!.*foo)/)
+      , result
+
+    expect(all('foo sth', Object.create(null))).to.equal('foo sth')
+    expect(all('bar sth', Object.create(null))).to.equal('bar sth')
+    expect(o.fn('foo sth', Object.create(null))).to.equal(null)
+    expect(o.fn('bar sth', Object.create(null))).to.equal('bar sth')
+  })
+
 })
